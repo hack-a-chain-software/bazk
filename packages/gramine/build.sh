39d2a87a0f21346862c5f3ea770cafb0c3a15e77
Mateus Santana <42101423+1Mateus@users.noreply.github.com>
2024-02-22 21:29:53 -0300

    feat(provisioning): rename worker

--- packages/gramine/build.sh
+++ packages/gramine/build.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
-(cd bazk-build && rm *.manifest && rm *.sgx && rm *.sig && rm -r cruntime && rm -r app && rm -r dist)
+(cd bazk-build && rm -f *.manifest *.sgx *.sig && rm -rf cruntime app dist)
 
 cp -r dist bazk-build/app
 cp -L $(which curl) bazk-build/
 
+# Run Docker without TTY in a non-interactive environment
 sudo docker run \
  -u $(id -u ${USER}):$(id -g ${USER}) \
- -it --rm \
+ --rm \
  -v $(pwd)/bazk-build:/bazk-build \
- --env IAS_SPID=$IAS_SPID \
- kvin/gramine:1.0 \
- "make dist -C /bazk-build"
+ --env IAS_SPID=$
