122f2e6fedb04265cac7a4ca571368e8a44571c8
Mateus Santana <42101423+1Mateus@users.noreply.github.com>
2024-02-27 20:38:39 -0300

    Update build.sh

--- packages/gramine/build.sh
+++ packages/gramine/build.sh
@@ -2,7 +2,8 @@
 (cd bazk-build && rm -f *.manifest *.sgx *.sig && rm -rf cruntime app dist)
 
 cp -r dist bazk-build/app
-# cp -L $(which curl) bazk-build/
+cp -L $(which curl) bazk-build/
+cp -L $(which node) bazk-build/
 
 # Run Docker without TTY in a non-interactive environment
 sudo docker run \
