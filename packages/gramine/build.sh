124b5fdbe7e5f22e98862ccdced771859e681152
Mateus Santana <42101423+1Mateus@users.noreply.github.com>
2024-02-27 22:38:41 -0300

    Update build.sh

--- packages/gramine/build.sh
+++ packages/gramine/build.sh
@@ -10,6 +10,6 @@ sudo docker run \
     -u $(id -u ${USER}):$(id -g ${USER}) \
     --rm \
     -v $(pwd)/bazk-build:/bazk-build \
-    --env .env \
+    --env IAS_SPID="2E0E3B2459C4AB70981163C09D4E1A59" \
     kvin/gramine:1.0 \
     "make dist -C /bazk-build"
