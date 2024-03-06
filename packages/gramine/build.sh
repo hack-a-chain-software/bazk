f284a5c9ee049788851fe1aeb4a3d801ba88ab28
Mateus Santana <42101423+1Mateus@users.noreply.github.com>
2024-03-06 16:01:08 -0300

    feat: ping

--- packages/gramine/build.sh
+++ packages/gramine/build.sh
@@ -2,7 +2,6 @@
 (cd bazk-build && rm -f *.manifest *.sgx *.sig && rm -rf cruntime app dist)
 
 cp -r dist bazk-build/app
-cp ./bazk-build/circuit.json bazk-build/app
 
 cp -L $(which curl) bazk-build/
 
@@ -14,3 +13,5 @@ sudo docker run \
     --env IAS_SPID=$IAS_SPID \
     kvin/gramine:1.0 \
     "make dist -C /bazk-build"
+
+cp ./bazk-build/circuit.json bazk-build/dist
