3fb3f0fb2231028e0c1b926e848a339a7e07f2fe
Mateus Santana <42101423+1Mateus@users.noreply.github.com>
2024-03-07 13:57:47 -0300

    feat: ping

--- packages/gramine/build.sh
+++ packages/gramine/build.sh
@@ -10,6 +10,9 @@ sudo docker run \
     -u $(id -u ${USER}):$(id -g ${USER}) \
     --rm \
     -v $(pwd)/bazk-build:/bazk-build \
+    -v /usr/share/zoneinfo/:/usr/share/zoneinfo/ \
+    -v /usr/share/zoneinfo-icu/:/usr/share/zoneinfo-icu/ \
+    -v /etc/localtime:/etc/localtime \
     --env IAS_SPID=$IAS_SPID \
     kvin/gramine:1.0 \
     "make dist -C /bazk-build"
