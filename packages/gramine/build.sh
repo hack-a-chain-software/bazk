27d202f7d105782be8c6ef073c7d879dc3e643af
1mateus <mateus4loading@gmail.com>
2024-03-17 23:39:52 -0300

    feat: update base

--- packages/gramine/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-sudo docker run \
-    -u $(id -u ${USER}):$(id -g ${USER}) \
-    --rm \
-    --env IAS_SPID=$IAS_SPID \
-    -v $(pwd)/bazk-build:/bazk-build \
-    mateus4loading/bazk-builder:latest \
