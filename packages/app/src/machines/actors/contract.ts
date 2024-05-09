8975f823a49c89261d35a912aff7405a74c18e99
1mateus <mateus4loading@gmail.com>
2024-05-08 22:29:39 -0300

    feat(app): new contract

--- packages/app/src/machines/actors/contract.ts
+++ packages/app/src/machines/actors/contract.ts
@@ -2,7 +2,7 @@ import abi from '@/abi.json'
 import { fromPromise } from "xstate";
 import { PinkContractPromise } from '@phala/sdk';
 
-const BAZK_CONTRACT_ID = '0x8113a143dc2b8d5e7b2ea82f2741c35228052360bb1a4deb3a57467fa76918ee'
+const BAZK_CONTRACT_ID = '0xd72ac19567088e35a446e0dacdfadc5f266ea68d2481401d9e6b374310fac3de'
 
 export const contractConnection = fromPromise(
   async ({ input }: any) => {
